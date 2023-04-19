a86e94d418f49ad20af6d827892f3cdf33fb08e5
Kareem Nabulsi <knabulsi@student.sdccd.edu>
2023-04-19 16:56:37 -0700

    Final Monolith code for my integer calculator with comments.

--- Client/src/main/java/edu/sdccd/cisc191/template/Client.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/Client.java
@@ -1,21 +1,17 @@
 package edu.sdccd.cisc191.template;
-
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
 
-import java.io.FileNotFoundException;
 import java.util.LinkedList;
 
 public class Client extends Application {
@@ -24,132 +20,17 @@ public class Client extends Application {
     private TextField textFieldPendingNum;
     private Label labelAnswer;
     private Label previousAnswer;
+    private Label maximumAnswer;
+    private Label minimumAnswer;
     private final Button[][] buttonStorage = new Button[3][3];
     private boolean isRunning = false;
-    private LinkedList<String> calcHistory = new LinkedList<>();
+    private LinkedList<String> calcHistory;
+    private int finalAnswer;
+    private int pendingNum;
+    private int currentNum;
 
 
-    public GridPane createButtonGrid() {
 
-        isRunning = true;
-        //make a new grid pane object
-        GridPane buttonPane = new GridPane();
-        //set vertical/horizontal gap
-        buttonPane.setHgap(0);
-        buttonPane.setVgap(0);
-        //set alignment
-        buttonPane.setAlignment(Pos.BOTTOM_CENTER);
-
-        // Create a grid of buttons and add them to the buttonPane
-        for (int r = 0; r < buttonStorage.length; r++) {
-            for (int c = 0; c < buttonStorage[r].length; c++) {
-                Button button = buttonStorage[r][c];
-                //if buttons are not null, then add to grid pane
-                if (button != null) {
-                    //set action handler for each button while creating a button logic object to handle logic for each action
-                    button.setOnAction(new ButtonLogic());
-                    buttonPane.add(button, c, r);
-                }
-            }
-        }
-
-        return buttonPane;
-    }
-
-    public class ButtonLogic implements EventHandler<ActionEvent> {
-
-        @Override
-        public void handle(ActionEvent event) {
-            isRunning = true;
-            //typecast to get the source of the button that was pressed
-
-
-            //get text of the button (operator)
-            //quit button
-            if (event.getSource() == buttonStorage[2][0]) {
-                IOHelper.writeData(calcHistory);
-                Platform.exit();
-                isRunning = false;
-            }
-            //if num is too big (over 9 places) print to console
-            int pendingNum;
-            int currentNum;
-            try {
-                //parse the user input from a String to int
-                currentNum = Integer.parseInt(textFieldCurrentNum.getText());
-                pendingNum = Integer.parseInt(textFieldPendingNum.getText());
-            } catch (NumberFormatException nfe) {
-                //if that fails, clear all text and print to console
-                textFieldCurrentNum.setText("");
-                textFieldPendingNum.setText("");
-                labelAnswer.setText("Too Large To Compute");
-                nfe.printStackTrace();
-                return;
-            }
-            //clear button
-
-            int finalAnswer;
-            if (event.getSource() == buttonStorage[2][1]) {
-                textFieldCurrentNum.setText("");
-                textFieldPendingNum.setText("");
-                labelAnswer.setText("AnswerBox");
-                previousAnswer.setText(calcHistory.getLast());
-            }
-            // addition button
-            else if (event.getSource() == buttonStorage[0][0]) {
-                finalAnswer = currentNum + pendingNum;
-                labelAnswer.setText(String.valueOf(finalAnswer));
-                previousAnswer.setText(calcHistory.getLast());
-                calcHistory.add(String.valueOf(finalAnswer));
-            }
-            // subtract button
-            else if (event.getSource() == buttonStorage[0][1]) {
-                finalAnswer = currentNum - pendingNum;
-                previousAnswer.setText(calcHistory.getLast());
-                labelAnswer.setText(String.valueOf(finalAnswer));
-                calcHistory.add(String.valueOf(finalAnswer));
-            }
-
-            /*
-            if(event.getSource() == buttonStorage[0][1]) {
-                int answer = calc.add(currentNum, pendingNum)
-             */
-            // multiply button
-            else if (event.getSource() == buttonStorage[1][0]) {
-                finalAnswer = currentNum * pendingNum;
-                previousAnswer.setText(calcHistory.getLast());
-                labelAnswer.setText(String.valueOf(finalAnswer));
-                calcHistory.add(String.valueOf(finalAnswer));
-
-            }
-            // divide button
-            else if (event.getSource() == buttonStorage[1][1]) {
-                //if its 0/0
-                if (pendingNum == 0) {
-                    labelAnswer.setText("Undefined");
-                    calcHistory.add("Undefined");
-                } else {
-                    finalAnswer = currentNum / pendingNum;
-                    previousAnswer.setText(calcHistory.getLast());
-                    labelAnswer.setText(String.valueOf(finalAnswer));
-                    calcHistory.add(String.valueOf(finalAnswer));
-                }
-            }
-
-        }
-    }
-
-    public void setLblAnswer(String str) {
-        labelAnswer.setText(str);
-    }
-
-    public void setTxtCurrentNum(String str) {
-        textFieldCurrentNum.setText(str);
-    }
-    public void setTxtPendingNum(String str) {
-        textFieldPendingNum.setText(str);
-    }
-    // Define a handler for button clicks
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -157,25 +38,25 @@ public class Client extends Application {
         if (isRunning) {
             throw new Exception("Rendering encountered an error. ");
         }
-        try {
-            calcHistory = IOHelper.readItems("output.txt");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
         //create text fields
         textFieldCurrentNum = new TextField();
         textFieldPendingNum = new TextField();
+
+        minimumAnswer = new Label("MinimumAnswer");
+        minimumAnswer.setAlignment(Pos.CENTER);
+        maximumAnswer = new Label("MaximumAnswer");
+        maximumAnswer.setAlignment(Pos.CENTER);
+
 
         //create labels and set alignment
         labelAnswer = new Label("AnswerBox");
         labelAnswer.setAlignment(Pos.CENTER);
         previousAnswer = new Label("Previous Answer");
-        previousAnswer.setText(calcHistory.getLast());
         previousAnswer.setAlignment(Pos.BOTTOM_RIGHT);
-        //calcHistory = new LinkedList();
+        calcHistory = new LinkedList();
 
         //set first/last value of linked list to no answers on start.
-        calcHistory.add(calcHistory.getLast());
+        calcHistory.add("No previous answers");
 
         //fill buttons accordingly
         buttonStorage[0][0] = new Button("+");
@@ -202,7 +83,7 @@ public class Client extends Application {
 
 
         // Create an HBox to hold the text fields and labels
-        HBox hBoxInput = new HBox(80, textFieldCurrentNum, textFieldPendingNum, labelAnswer, previousAnswer);
+        HBox hBoxInput = new HBox(80, textFieldCurrentNum, textFieldPendingNum, labelAnswer, previousAnswer, maximumAnswer, minimumAnswer);
         hBoxInput.setPrefHeight(50);
         hBoxInput.setAlignment(Pos.BOTTOM_CENTER);
 
@@ -221,8 +102,190 @@ public class Client extends Application {
 
     }
 
-    public static void main(String[] args) {
+    public GridPane createButtonGrid() {
+        isRunning = true;
+        //make a new gridpane object
+        GridPane buttonPane = new GridPane();
+        //set vertical/horizontal gap
+        buttonPane.setHgap(0);
+        buttonPane.setVgap(0);
+        //set alignment
+        buttonPane.setAlignment(Pos.BOTTOM_CENTER);
+
+        // Create a grid of buttons and add them to the buttonPane
+        for (int r = 0; r < buttonStorage.length; r++) {
+            for (int c = 0; c < buttonStorage[r].length; c++) {
+                Button button = buttonStorage[r][c];
+                //if buttons are not null, then add to gridpane
+                if (button != null) {
+                    //set action handler for each button while creating a button logic object to handle logic for each action
+                    button.setOnAction(new ButtonLogic());
+                    buttonPane.add(button, c, r);
+                }
+            }
+        }
+
+        return buttonPane;
+    }
+
+    public Label getLblAnswer() {
+        return labelAnswer;
+    }
+
+    public void setLblAnswer(String str) {
+        labelAnswer.setText(str);
+    }
+
+    public Label getLblPrevAnswer() {
+        return previousAnswer;
+    }
+
+    public void setLblPrevAnswer(String str) {
+        previousAnswer.setText(str);
+    }
+
+    public int getPendingNum() {
+        return pendingNum;
+    }
+
+    public int getCurrentNum() {
+        return currentNum;
+    }
+
+    public void setTxtCurrentNum(String str) {
+        textFieldCurrentNum.setText(str);
+    }
+
+    public void setTxtPendingNum(String str) {
+        textFieldPendingNum.setText(str);
+    }
+
+
+    // Define a handler for button clicks
+    public class ButtonLogic implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            isRunning = true;
+
+            //typecast to get the source of the button that was pressed
+
+
+            //get text of the button (operator)
+            //quit button
+            if(event.getSource() == buttonStorage[2][0]) {
+                Platform.exit();
+                isRunning = false;
+            }
+            //if num is too big (over 9 places) print to console
+            try {
+                //parse the user input from a String to int
+                currentNum = Integer.parseInt(textFieldCurrentNum.getText());
+                pendingNum = Integer.parseInt(textFieldPendingNum.getText());
+            }
+            catch(NumberFormatException nfe) {
+                //if that fails, clear all text and print to console
+                textFieldCurrentNum.setText("");
+                textFieldPendingNum.setText("");
+                labelAnswer.setText("Too Large To Compute");
+                nfe.printStackTrace();
+                return;
+            }
+            //clear button
+
+            if(event.getSource() == buttonStorage[2][1]) {
+                textFieldCurrentNum.setText("");
+                textFieldPendingNum.setText("");
+                labelAnswer.setText("AnswerBox");
+                previousAnswer.setText("" + calcHistory.getLast());
+            }
+            // addition button
+            else if(event.getSource() == buttonStorage[0][0]) {
+                finalAnswer = currentNum + pendingNum;
+                labelAnswer.setText("" + finalAnswer);
+                previousAnswer.setText("" + calcHistory.getLast());
+                calcHistory.add("" + finalAnswer);
+                if(calcHistory.size() > 2) {
+                    String previousNum = (String) calcHistory.get(calcHistory.size() - 2);
+                    int previousNumInt = Integer.parseInt(previousNum);
+                    if (finalAnswer > previousNumInt) {
+                        maximumAnswer.setText("" + finalAnswer);
+                    }
+                    if (finalAnswer < previousNumInt){
+                        minimumAnswer.setText("" + finalAnswer);
+                    }
+                }
+            }
+            // subtract button
+            else if(event.getSource() == buttonStorage[0][1]) {
+                finalAnswer = currentNum - pendingNum;
+                previousAnswer.setText("" + calcHistory.getLast());
+                labelAnswer.setText("" + finalAnswer);
+                calcHistory.add("" + finalAnswer);
+                if(calcHistory.size() > 2) {
+                    String previousNum = (String) calcHistory.get(calcHistory.size() - 2);
+                    int previousNumInt = Integer.parseInt(previousNum);
+                    if (finalAnswer > previousNumInt) {
+                        maximumAnswer.setText("" + finalAnswer);
+                    }
+                    if (finalAnswer < previousNumInt){
+                        minimumAnswer.setText("" + finalAnswer);
+                    }
+                }
+            }
+
+            /*
+            if(event.getSource() == buttonStorage[0][1]) {
+                int answer = calc.add(currentNum, pendingNum)
+             */
+            // multiply button
+            else if(event.getSource() == buttonStorage[1][0]) {
+                finalAnswer = currentNum * pendingNum;
+                previousAnswer.setText("" + calcHistory.getLast());
+                labelAnswer.setText("" + finalAnswer);
+                calcHistory.add("" + finalAnswer);
+                if(calcHistory.size() > 2) {
+                    String previousNum = (String) calcHistory.get(calcHistory.size() - 2);
+                    int previousNumInt = Integer.parseInt(previousNum);
+                    if (finalAnswer > previousNumInt) {
+                        maximumAnswer.setText("" + finalAnswer);
+                    }
+                    if (finalAnswer < previousNumInt){
+                        minimumAnswer.setText("" + finalAnswer);
+                    }
+                }
+
+            }
+            // divide button
+            else if(event.getSource() == buttonStorage[1][1]) {
+                //if its 0/0
+                if(pendingNum == 0) {
+                    labelAnswer.setText("Undefined");
+                    calcHistory.add("Undefined");
+                }
+                else {
+                    finalAnswer = currentNum / pendingNum;
+                    previousAnswer.setText("" + calcHistory.getLast());
+                    labelAnswer.setText("" + finalAnswer);
+                    calcHistory.add("" + finalAnswer);
+                    if(calcHistory.size() > 2) {
+                        String previousNum = (String) calcHistory.get(calcHistory.size() - 2);
+                        int previousNumInt = Integer.parseInt(previousNum);
+                        if (finalAnswer > previousNumInt) {
+                            maximumAnswer.setText("" + finalAnswer);
+                        }
+                        if (finalAnswer < previousNumInt){
+                            minimumAnswer.setText("" + finalAnswer);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    public static void main (String[]args){
         //run code
-        launch();
+        launch();  // Run this Application.
+
     }
 }
