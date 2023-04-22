8ba8a38fefab4f0af5e4a1406d6d66c951fb50e4
lebed <lebed.kevin@gmail.com>
2023-04-21 23:30:46 -0700

    Revert "Networking implementation for socket and serialization."
    This reverts commit 7aa0ee69347507ef4104589fb8f3d4d61cb363f0.

--- Client/src/main/java/edu/sdccd/cisc191/template/CalcHistory.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/CalcHistory.java
@@ -2,5 +2,5 @@ package edu.sdccd.cisc191.template;
 
 public class CalcHistory {
     //TODO Move linked list to here and all assosciated methods along with it.
-    //make default constructorr
+    //make default constructor
 }
